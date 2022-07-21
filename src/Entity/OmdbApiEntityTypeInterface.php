8eda86fa0b22c447d7225357ce95e9213e0addb6
Bhanu951 <Bhanu951@3607219.no-reply.drupal.org>
2022-07-21 13:14:50 +0530

    #3291606: Entity Type Interface Added.

---
+++ src/Entity/OmdbApiEntityTypeInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\omdb_api\Entity;
+
+use Drupal\Core\Config\Entity\ConfigEntityInterface;
+
+/**
+ * Provides an interface for defining OMDB API Type Entities.
+ */
+interface OmdbApiEntityTypeInterface extends ConfigEntityInterface {
+
+  // Add get/set methods for your configuration properties here.
+}
